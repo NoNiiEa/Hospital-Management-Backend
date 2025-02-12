c6abd14946da69914b028785882373504a3288a9
NoNiiEa <chanonben4@gmail.com>
2025-02-12 21:41:01 +0700

    Minor change in main function

--- routes/routes.py
+++ routes/routes.py
@@ -12,21 +12,3 @@ router.include_router(doctor_router, prefix="/doctors", tags=["doctors"])
 router.include_router(appointment_router, prefix="/appointments", tags=["appointments"])
 
 
-# @router.get("/todos")
-# async def get_todos():
-#     todos = collection_name.find()
-#     return list_schema(todos)
-
-# @router.post("/todos")
-# async def create_todos(todo: TodoModel):
-#     response = collection_name.insert_one(dict(todo))
-#     return {"id": str(response.inserted_id)}
-
-# @router.get("/todos/{id}")
-# async def get_todo_by_id(id: str):
-#     todo = collection_name.find_one({"_id": ObjectId(id)})
-#     if todo:
-#         return individual_schema(todo)
-#     raise HTTPException(status_code=404, detail="Todo not found")
-
-
