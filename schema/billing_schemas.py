3662ba6af0e1bcccc3ba69fc0d94f05cae6a57c4
NoNiiEa <chanonben4@gmail.com>
2025-02-14 13:26:51 +0700

    minor change

--- schema/billing_schemas.py
+++ schema/billing_schemas.py
@@ -3,8 +3,8 @@ from bson import ObjectId
 def individual_billing_schema(billing):
     return {
         "id": str(billing["_id"]),
-        "patient": billing.get("patient_id","N\A"),
-        "appointment": billing.get("appointment_id","N\A"),
+        "patient": billing.get("patient_id","N/A"),
+        "appointment": billing.get("appointment_id","N/A"),
         "total_amount": billing.get("total_amount", "N/A"),
         "payment_method":billing.get("payment_method", "N/A")
         
