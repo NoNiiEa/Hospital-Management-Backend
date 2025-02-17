dc627ccf2b66bc52cccc3929b26683ea9b5cd874
NoNiiEa <chanonben4@gmail.com>
2025-02-17 15:17:59 +0700

    disable cors for develop

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
