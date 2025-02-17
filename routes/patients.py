ae875a35fe95927e27de77dffd7ef772a69e4071
NoNiiEa <chanonben4@gmail.com>
2025-02-17 16:10:27 +0700

    Hotfix patient.py

--- routes/patients.py
+++ routes/patients.py
@@ -22,7 +22,7 @@ async def get_patient_individal(patient_id: str):
     if not ObjectId.is_valid(patient_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid patient id.")
 
-    patient =await patients_collection.find_one({"_id": ObjectId(patient_id)})
+    patient = patients_collection.find_one({"_id": ObjectId(patient_id)})
     
     if not patient:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Patient not found.")
