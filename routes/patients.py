daef8ffdbb5ac2cd23e5bcec0c1c133d5e0861ce
NoNiiEa <chanonben4@gmail.com>
2025-02-19 11:48:11 +0700

    add medicalHistory request for patient.

--- routes/patients.py
+++ routes/patients.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from models.patients import PatientModel, GetPatientRequest
+from models.patients import PatientModel, GetPatientRequest, MedicalHistoryModel
 from config.database import (
     patients as patients_collection, 
     doctors as doctor_collection, 
@@ -44,6 +44,30 @@ async def get_patientsLimit(request: GetPatientRequest):
 async def create_patients(patient: PatientModel):
     response = patients_collection.insert_one(patient.model_dump())
     return {"id": str(response.inserted_id)}
+
+@patient_router.put("/medicalhistory/{patient_id}/")
+async def add_medical_history(patient_id: str, medical_history: MedicalHistoryModel):
+    if not ObjectId.is_valid(patient_id):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid patient ID.")
+
+    patient = patients_collection.find_one({"_id": ObjectId(patient_id)})
+    
+    if not patient:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Patient not found.")
+
+    medical_history_dict = medical_history.dict()  # Convert Pydantic model to dictionary
+
+    result = patients_collection.update_one(
+        {"_id": ObjectId(patient_id)},
+        {"$push": {"medical_history": {"$each": [medical_history_dict], "$position": 0}}}
+    )
+
+    if result.modified_count == 0:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to update medical history.")
+
+    return {"message": "Successfully updated medical history."}
+
+    
 
 @patient_router.delete("/delete/{patient_id}", status_code=status.HTTP_200_OK)
 async def delete_patient(patient_id: str):
