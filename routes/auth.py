e081dc935808447a80d729302dae38bd63df5663
NoNiiEa <chanonben4@gmail.com>
2025-02-23 15:23:42 +0700

    make docker component

--- routes/auth.py
+++ routes/auth.py
@@ -14,8 +14,6 @@ import os
 env_file = ".env"
 if os.path.exists(env_file):
     load_dotenv(env_file)
-else:
-    print("⚠️ Warning: .env file not found! Ensure it exists before running the app.")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
